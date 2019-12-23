49820430bf8351192f23d544fd173ef75cf18bdd
Ryan McDonagh <ryan@henschen.com>
2019-12-23 16:56:42 -0500

    added signin component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,15 +10,42 @@ const config = {
     storageBucket: "crown-clothing-80400.appspot.com",
     messagingSenderId: "709280830189",
     appId: "1:709280830189:web:e7f648162241a81a5172e4"
-  };
+};
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+    const snapShot = await userRef.get();
 
-  export default firebase;
+    if(!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
