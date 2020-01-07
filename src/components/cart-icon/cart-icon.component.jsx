4d482161f7de9888d46a87258f92b53f9caa99a0
Ryan McDonagh <ryan@henschen.com>
2020-01-07 17:00:33 -0500

    working on converting styles to styled components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,13 @@ import { createStructuredSelector } from 'reselect';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import './cart-icon.styles.scss';
+import { CartIconContainer, ShoppingIcon, ItemCountContainer } from './cart-icon.styles';
 
 const CartIcon = ({ toggleCartHidden, itemsCount }) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>{itemsCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+        <ShoppingIcon />
+        <ItemCountContainer>{itemsCount}</ItemCountContainer>
+    </CartIconContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,24 +3,24 @@ import { connect } from 'react-redux';
 
 import { clearItem, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-import './checkout-item.styles.scss';
+import { CheckoutItemContainer, ImageContainer, NameContainer, QuantityContainer, RemoveButtonContainer } from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem, clearCart, addItem, removeItem }) => {
     const { name, imageUrl, quantity, price } = cartItem;
     return (
-        <div className='checkout-item'>
-            <div className='image-container'>
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt="item" />
-            </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
-                <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className='remove-button' onClick={() => clearCart(cartItem)}>&#10005;</div>
-        </div>
+            </ImageContainer>
+            <NameContainer>{name}</NameContainer>
+            <QuantityContainer>
+                <div onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <span>{quantity}</span>
+                <div onClick={() => addItem(cartItem)}>&#10095;</div>
+            </QuantityContainer>
+            <NameContainer>{price}</NameContainer>
+            <RemoveButtonContainer onClick={() => clearCart(cartItem)}>&#10005;</RemoveButtonContainer>
+        </CheckoutItemContainer>
     )
 };
 
