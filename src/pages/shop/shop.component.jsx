ca76dc8976eca93b1d343600ea99e1503a05328b
Ryan McDonagh <ryan@henschen.com>
2020-01-09 16:33:05 -0500

    added loading spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,12 +3,19 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { updateCollections } from '../../redux/shop/shop.actions';
 
-import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import WithSpinner from '../../components/withSpinner/with-spinner.component';
 
 import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+    state = {
+        isLoading: true
+    }
     unsubscribeFromSnapsot = null;
 
     componentDidMount() {
@@ -17,15 +24,17 @@ class ShopPage extends React.Component {
         this.unsubscribeFromSnapsot = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({ loading: false })
         })
     }
 
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionOverview} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} render={props => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
+                <Route path={`${match.path}/:collectionId`} render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
             </div>
         )
     }
