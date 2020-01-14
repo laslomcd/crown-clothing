e051abe107d6e9fba99bdce77c11d18dd68c5abc
Ryan McDonagh <ryan@henschen.com>
2020-01-14 08:21:51 -0500

    started redux thunk

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,14 +5,19 @@ const selectShop = state => state.shop
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
-)
+);
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     collections => collections ? Object.keys(collections).map(key => collections[key]) : []
-)
+);
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
     collections => (collections ? collections[collectionUrlParam] : null)
-)
+);
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
